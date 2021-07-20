5f7c248ff6f6ea4b421c32094f623d2a105f2f32
Raphael Barbosa Rodrigues <raphaelbarbosa.rodrigues@gmail.com>
2021-07-20 08:27:07 -0300

    setup: installing node versions

--- scripts/installation/javascript.sh
+++ scripts/installation/javascript.sh
@@ -10,6 +10,10 @@ sudo apt install npm;
 npm install -g yarn;
 npm install -g nodemon;
 
+nvm install 14.15.1;
+nvm install 10.17.0;
+nvm install 8.15.0;
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash;
 
 
