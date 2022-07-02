33096ff9023f52252846019fc3030d2ce32dc8ab
Raphael Barbosa Rodrigues <raphaelbarbosa.rodrigues@gmail.com>
2022-07-01 23:49:48 -0300

    feat: creating fedora basic config

--- scripts/config/vtex.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl https://raw.githubusercontent.com/vtex/toolbelt/master/plugins/bash/vtex.bash >> ~/.bashrc
-curl https://raw.githubusercontent.com/vtex/toolbelt/master/plugins/bash/prompt.bash >> ~/.bashrc
--- scripts/config/wallpaper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gsettings set org.gnome.desktop.background picture-uri https://raw.githubusercontent.com/RaphaelBRodrigues/ubuntu_setup/master/assets/dark_wallpaper.png
-
