17100d85cd62419a6d3a4516353ba3453a1c878a
Raphael Barbosa Rodrigues <raphaelbarbosa.rodrigues@gmail.com>
2022-07-02 18:04:22 -0300

    improving fedora script

--- fedora/setup.sh
+++ fedora/setup.sh
@@ -17,7 +17,9 @@ sudo dnf install google-chrome-stable -y
 sudo dnf install -y nodejs 
 sudo dnf install -y docker 
 sudo dnf install -y deepin-terminal 
-sudo dnf install -y vim;
+sudo dnf install -y vim
+sudo dnf -y install openvpn
+
 
 sudo dnf install -y docker
 sudo dnf install -y docker-compose
@@ -28,6 +30,9 @@ sudo snap install spotify
 sudo snap install discord 
 sudo snap install insomnia 
 sudo snap install --classic code;
+
+sudo dnf install -y NetworkManager-openvpn-gnome openvpn network-manager-openvpn network-manager-openvpn-gnome network-manager-vpnc;
+
 
 wget -O dbeaver.rpm https://download.dbeaver.com/community/22.1.1/dbeaver-ce-22.1.1-stable.x86_64.rpm 
 sudo rpm -i dbeaver.rpm
@@ -42,6 +47,15 @@ sudo sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://packageclo
 sudo dnf install github-desktop -y
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash;
+
+sudo /etc/init.d/networking restart
+
+mkdir .OpenVPN
+
+sudo dnf install -y python3-pip
+sudo python3 -m pip install localstack
+
+sudo dnf install -y eb
 
 clear;
 echo "Star the repository https://github.com/RaphaelBRodrigues/linux-setup :)"
