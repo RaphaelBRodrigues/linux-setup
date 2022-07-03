913d33f7b815979b3386abb259e4fabc10eb076a
Raphael Barbosa Rodrigues <raphaelbarbosa.rodrigues@gmail.com>
2022-07-03 15:30:43 -0300

    removing recursive entry call

--- fedora/scripts/installation/entry.sh
+++ fedora/scripts/installation/entry.sh
@@ -6,7 +6,6 @@ curl -o- $BASE_URL/scripts/installation/general.sh | bash;
 curl -o- $BASE_URL/scripts/installation/aws.sh | bash;
 curl -o- $BASE_URL/scripts/installation/db.sh | bash;
 curl -o- $BASE_URL/scripts/installation/docker.sh | bash;
-curl -o- $BASE_URL/scripts/installation/entry.sh | bash;
 curl -o- $BASE_URL/scripts/installation/node.sh | bash;
 curl -o- $BASE_URL/scripts/installation/snap.sh | bash;
 
