5486b5382d62c2c2591d86d73029a92bd3ee44f2
Raphael Barbosa Rodrigues <raphaelbarbosa.rodrigues@gmail.com>
2022-07-03 22:09:48 -0300

    adding env on setup script

--- fedora/setup.sh
+++ fedora/setup.sh
@@ -7,6 +7,7 @@ echo "      Starting setup       ";
 echo "###########################"
 
 curl -o- https://raw.githubusercontent.com/RaphaelBRodrigues/linux-setup/master/fedora/scripts/installation/entry.sh | bash;
+curl -o- https://raw.githubusercontent.com/RaphaelBRodrigues/linux-setup/master/fedora/scripts/env/entry.sh | bash;
 
 clear;
 echo "Star the repository https://github.com/RaphaelBRodrigues/linux-setup :)"
