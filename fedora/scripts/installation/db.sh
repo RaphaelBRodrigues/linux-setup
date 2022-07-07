e20d167d00360476049972ca1e5f2b9623dedfd0
Raphael Barbosa Rodrigues <raphaelbarbosa.rodrigues@gmail.com>
2022-07-07 17:46:03 -0300

    adding mongo compass

--- fedora/scripts/installation/db.sh
+++ fedora/scripts/installation/db.sh
@@ -8,6 +8,10 @@ wget -O dbeaver.rpm https://download.dbeaver.com/community/22.1.1/dbeaver-ce-22.
 sudo rpm -i dbeaver.rpm
 rm dbeaver.rpm
 
+wget -O mongo.rpm https://downloads.mongodb.com/compass/mongodb-compass-readonly-1.28.4.x86_64.rpm 
+sudo rpm -i mongo.rpm
+rm mongo.rpm
+
 echo "################################";
 echo "      DB Script Finished      ";
 echo "################################";
